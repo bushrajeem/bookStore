7a382ba6b5adac594de538cd10fc89426620722a
bushrajeem <bushrajeem32@gmail.com>
2025-05-24 19:38:21 +0600

    made card component

---
+++ client/src/feature/comment/component/CommentCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Card, CardHeader } from 'src/shadcnUI/components/ui/card'
+
+function CommentCard() {
+  return (
+    <div>
+      <Card>
+        <CardHeader>
+        </CardHeader>
+      </Card>
+    </div>
+  )
+}
+
+export default CommentCard
---
+++ client/src/feature/comment/index.js
@@ -0,0 +1,1 @@
+import { Comment } from "../comment/component/CommentCard.jsx";
